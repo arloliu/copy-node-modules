7330c5b49ede04cf01a5e19114c1ea7e446bff7a
Arlo Liu <arlo.liu@atrustcorp.com>
2016-05-13 01:30:38 +0800

    CLI: Modify input argument for CLI program

--- bin/copy-node-modules.js
+++ bin/copy-node-modules.js
@@ -1,59 +1,30 @@
 #!/usr/bin/env node
 var path = require('path');
-var _ = require('lodash');
 var copyNodeModules = require('../');
 var args = process.argv.slice(2);
 
 if (args.length < 2)
 {
-    console.error('Usage: copy-node-module src_dir dest_dir [--dev=bool] [-v|--verbose] ');
+    console.error('Usage: copy-node-module src_dir dest_dir [--dev] [-v|--verbose] ');
     process.exit(1);
 }
 
 var srcDir = args[0].trim(),
     dstDir = args[1].trim(),
-    deps = true,
     devDeps = false,
     verbose = false;
 
-
-function getArgumentValue(args, option)
-{
-    var index = _.findIndex(args, function(arg) {
-        return _.startsWith(arg, option);
-    });
-    if (index < 0)
-        return null;
-    var token = args[index].split('=', 2);
-    console.dir(token);
-    if (token.length !== 2)
-        return null;
-    return token[1];
-}
-
-function parseBooleanValue(val)
-{
-    val = val.toLowerCase();
-    if (val == 'true' || val == '1' || val == 'yes' || val == 'y')
-        return true;
-    return false;
-}
-
 // parse input arguments
-var argValue;
-argValue = getArgumentValue(args, '--dev');
-if (argValue)
-    devDeps = parseBooleanValue(argValue);
-
+if (args.indexOf('--dev') !== -1)
+    devDeps = true;
 if (args.indexOf('-v') !== -1 || args.indexOf('--verbose') !== -1)
     verbose = true;
 
-if (verbose)
-{
-    console.log('srcDir = ' + srcDir);
-    console.log('dstDir = ' + dstDir);
-    console.log('--dev  = ' + devDeps);
-}
+if (!path.isAbsolute(srcDir))
+    srcDir = path.resolve(process.cwd(), srcDir);
+if (!path.isAbsolute(srcDir))
+    dstDir = path.resolve(process.cwd(), dstDir);
+
 
 var options = {devDependencies: devDeps};
 copyNodeModules(srcDir, dstDir, options, function(err, packages) {
